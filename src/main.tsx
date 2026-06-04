e561ba9d51dfad295f534cc735a3b3d95daa526e
slemppa <sami@mak8r.fi>
2026-06-04 19:14:22 +0300

    feat: add BlogPost page and wire router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router'
 import './index.css'
-import App from './App.tsx'
+import Home from './pages/Home.tsx'
+import BlogList from './pages/BlogList.tsx'
+import BlogPost from './pages/BlogPost.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<BlogList />} />
+        <Route path="/blog/:slug" element={<BlogPost />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
