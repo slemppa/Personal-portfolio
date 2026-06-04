0d1e5e1bae34e2f5d2cbf430bfbfb149aa2a5be2
slemppa <sami@mak8r.fi>
2026-06-04 19:13:29 +0300

    refactor: move portfolio body into Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,25 @@
+import Nav from '../components/Nav'
+import Hero from '../components/Hero'
+import Cases from '../components/Cases'
+import TechStack from '../components/TechStack'
+import YouTube from '../components/YouTube'
+import Story from '../components/Story'
+import Contact from '../components/Contact'
+import Footer from '../components/Footer'
+
+export default function Home() {
+  return (
+    <>
+      <Nav />
+      <main>
+        <Hero />
+        <Cases />
+        <TechStack />
+        <YouTube />
+        <Story />
+        <Contact />
+      </main>
+      <Footer />
+    </>
+  )
+}
